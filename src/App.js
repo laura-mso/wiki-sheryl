9ce57817c952eb87f27426b859e17fe3f979c1d5
Laura Sochaczewski <laura.socha@gmx.net>
2020-05-25 16:39:26 +0200

    First english version

--- src/App.js
+++ src/App.js
@@ -1,26 +1,38 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
 import React from 'react';
-import logo from './logo.svg';
+import { useAsync } from 'react-async-hook';
+import ReactHtmlParser from 'react-html-parser';
 import './App.css';
 
+// Alternative link, saved for later: // 'https://en.wikipedia.org/w/api.php?action=query&origin=*&prop=extracts&explaintext&pageids=1624080&format=json'
+
+const baseUrl = 'https://en.wikipedia.org';
+const url =
+    baseUrl +
+    '/w/api.php?' +
+    new URLSearchParams({
+        origin: '*',
+        action: 'parse',
+        disableeditsection: true,
+        pageid: '1624080',
+        format: 'json',
+    });
+
+const fetchSherylData = async () => {
+    const data = await fetch(url);
+    const jsonData = await data.json();
+    return jsonData.parse.text['*'];
+};
+
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const data = useAsync(fetchSherylData, []);
+
+    return (
+        <div className="App">
+            {data.loading && 'Loading...'}
+            {data.result && ReactHtmlParser(data.result)}
+        </div>
+    );
 }
 
 export default App;
