9b127c12e217cf745a59abec90ecbe282651e70a
Laura Sochaczewski <laura.socha@gmx.net>
2020-05-28 15:58:35 +0200

    Getting started with i18n

--- src/App.js
+++ src/App.js
@@ -1,30 +1,28 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
-import detectBrowserLanguage from 'detect-browser-language';
-import React, { useState } from 'react';
+import i18next from 'i18next';
+import React from 'react';
 import { useAsync } from 'react-async-hook';
 import { Button } from 'react-bootstrap';
+import { useTranslation } from 'react-i18next';
 import './App.css';
 
-const languages = ['de', 'en', 'es', 'fr', 'it', 'nl', 'pl', 'pt', 'sv', 'tr'];
+const languageCodes = ['de', 'en', 'es', 'fr', 'it', 'nl', 'pl', 'pt', 'sv', 'tr'];
 
 function App() {
+    const { t, i18n } = useTranslation();
     // Feel free to replace the topic with "Albert Einstein"
     const topic = 'Sheryl Sandberg';
-    const setInitialLanguage = () => {
-        let language = detectBrowserLanguage().slice(0, 2);
-        language = 'de';
-        return languages.includes(language) ? language : 'en';
-    };
-    const [selectedLanguage, setselectedLanguage] = useState(setInitialLanguage());
 
-    const baseUrl = `https://${selectedLanguage}.wikipedia.org`;
+    let languageCode = i18next.language;
+
+    const baseUrl = `https://${languageCode}.wikipedia.org`;
     const url =
         baseUrl +
         '/w/api.php?' +
         new URLSearchParams({
             origin: '*',
             action: 'parse',
-            disableeditsection: true,
+            disableeditsection: 'true',
             page: topic,
             format: 'json',
         });
@@ -33,29 +31,26 @@ function App() {
         const data = await fetch(url);
         const jsonData = await data.json();
         // Hacky way to fix the external wikipedia links:
-        const result = jsonData.parse.text['*'].replace(
-            /href="\//g,
-            `href="https://${selectedLanguage}.wikipedia.org/`
-        );
+        const result = jsonData.parse.text['*'].replace(/href="\//g, `href="https://${languageCode}.wikipedia.org/`);
         return result;
     };
 
-    const data = useAsync(fetchSherylData, [selectedLanguage]);
-    console.log(data);
+    const data = useAsync(fetchSherylData, [languageCode]);
 
     return (
         <div className="container">
             <div className="card my-2">
                 <div className="card-body mx-auto">
+                    <p>Translation test: {t('title')}</p>
                     <h2>
                         Wikipedia: <span>{topic}</span>
                     </h2>
                     <div className="buttonContainer">
-                        {languages.map((language) => (
+                        {languageCodes.map((language) => (
                             <Button
-                                className={selectedLanguage === language ? 'active' : null}
+                                className={language === languageCode ? 'active' : undefined}
                                 key={language}
-                                onClick={() => setselectedLanguage(language)}
+                                onClick={() => i18n.changeLanguage(language)}
                             >
                                 {language}
                             </Button>
